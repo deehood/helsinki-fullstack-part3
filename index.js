e2bdca6a4af9796743c5729174a7bcc1fc01d124
deehood <dee.hood@gmail.com>
2022-03-28 16:54:37 +0100

    3.3: Phonebook backend step3  (deleted stuff remaining from notes)

--- index.js
+++ index.js
@@ -42,41 +42,8 @@ app.get("/api/persons", (req, res) => {
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((person) => person.id === id);
-
-  person ? res.json(person) : res.status(404).end();
-});
-
-const generateId = () => {
-  const newId = persons.length > 0 ? Math.max(...persons.map((x) => x.id)) : 0;
-  return newId + 1;
-};
-
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-
-  console.log(body);
-  if (!body.name) {
-    return res.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(person);
-
-  res.json(person);
-});
-
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+  console.log(person);
+  person ? res.send(person) : res.status(404).json({ error: "not found ..." });
 });
 
 const PORT = 3001;

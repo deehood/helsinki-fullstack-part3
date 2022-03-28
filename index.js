0dea8168ef2bbd20c9764052bfe549a3963b29c0
deehood <dee.hood@gmail.com>
2022-03-28 17:03:52 +0100

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ app.get("/api/persons/:id", (req, res) => {
   person ? res.send(person) : res.status(404).json({ error: "not found ..." });
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

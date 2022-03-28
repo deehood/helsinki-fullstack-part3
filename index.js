83662c7c63e3d9ee41dd92f24f0d640e79e1f639
deehood <dee.hood@gmail.com>
2022-03-28 16:24:25 +0100

    3.1: Phonebook backend step1

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
 const express = require("express");
 const app = express();
 
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>hey there ...</h1>");
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
+const generateId = () => {
+  const newId = persons.length > 0 ? Math.max(...persons.map((x) => x.id)) : 0;
+  return newId + 1;
+};
 
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
-  response.json(note);
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+  if (!body.name) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+  res.status(204).end();
 });
 
-app.post("/api/notes/", (request, response) => {
-  const note = request.body;
-  console.log(note);
-  response.json(note);
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  person ? res.json(person) : res.status(404).end();
 });
 
 const PORT = 3001;
